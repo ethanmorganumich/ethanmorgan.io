c740e77cb7536302dde382993e3ba6ae545991c5
ethanmorganumich <etcmo@umich.edu>
2022-11-27 22:53:12 -0500

    modify blog1

--- blog/compileJekyll.sh
+++ blog/compileJekyll.sh
@@ -1,3 +1,4 @@
+docker start elated_mcclintock
 docker exec bf309d47ce5334fba515606730782cfcd28b62776b76a90b23af2c027a907929 /bin/sh -c "cd /workspaces/blog; JEKYLL_ENV=production bundle exec jekyll b;";
 rm -rf /Users/emorgan/Documents/PersonalWebsite/blog/blog
 mv _site blog;
