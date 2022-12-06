e5cd9c1553356e62cd5d62265e5165b2d90f5557
ethanmorganumich <etcmo@umich.edu>
2022-12-06 13:56:37 -0500

    added chatGPT blog post

---
+++ blog/blog/blog/blog/blog/blog/blog/blog/blog/blog/blog/blog/blog/compileJekyll.sh
@@ -0,0 +1,6 @@
+docker start elated_mcclintock
+docker exec bf309d47ce5334fba515606730782cfcd28b62776b76a90b23af2c027a907929 /bin/sh -c "cd /workspaces/blog; JEKYLL_ENV=production bundle exec jekyll b;";
+rm -rf /Users/emorgan/Documents/PersonalWebsite/blog/blog
+mv _site blog;
+rm -rf /Users/emorgan/Documents/PersonalWebsite/ethanmorgan.io/blog
+cp -r /Users/emorgan/Documents/PersonalWebsite/blog/blog /Users/emorgan/Documents/PersonalWebsite/ethanmorgan.io/
