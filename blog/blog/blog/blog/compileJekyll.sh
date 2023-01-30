0dbdcc3af1ff2ab5051f0ea111806d12fe79a5df
ethanmorganumich <etcmo@umich.edu>
2023-01-30 15:01:08 -0500

    update Jan 30

--- blog/blog/blog/blog/compileJekyll.sh
+++
@@ -1,7 +0,0 @@
-docker start elated_mcclintock
-docker exec bf309d47ce5334fba515606730782cfcd28b62776b76a90b23af2c027a907929 /bin/sh -c "cd /workspaces/blog; JEKYLL_ENV=production bundle exec jekyll b;";
-docker stop elated_mcclintock 
-rm -rf /Users/emorgan/Documents/PersonalWebsite/blog/blog
-mv _site blog;
-rm -rf /Users/emorgan/Documents/PersonalWebsite/ethanmorgan.io/blog
-cp -r /Users/emorgan/Documents/PersonalWebsite/blog/blog /Users/emorgan/Documents/PersonalWebsite/ethanmorgan.io/
--- blog/blog/blog/blog/test_run.sh
+++
@@ -1,5 +0,0 @@
-docker start elated_mcclintock;
-docker exec bf309d47ce5334fba515606730782cfcd28b62776b76a90b23af2c027a907929 /bin/sh -c "cd /workspaces/blog; JEKYLL_ENV=production bundle exec jekyll b;";
-docker stop elated_mcclintock;
-open http://127.0.0.1:8080/blog/;
-http-server;
