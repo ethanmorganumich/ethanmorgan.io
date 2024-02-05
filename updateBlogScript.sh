215980d106c3aea3e442aeedb707536c99f9d412
ethanmorganumich <etcmo@umich.edu>
2024-02-04 23:40:42 -0800

    Update blog

---
+++ updateBlogScript.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+# Format the date as YYYY-MM-DD. You can adjust the date format as needed.
+TODAYS_DATE=$(date +%m-%d-%Y)
+
+npx quartz build -o blog
+git add blog
+git commit -m "Blog update $TODAYS_DATE"
