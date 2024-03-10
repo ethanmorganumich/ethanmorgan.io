6797c0a423c1cc2e0ae4bf788938c45356268f4b
ethanmorganumich <etcmo@umich.edu>
2024-03-09 18:30:05 -0800

    Remove Projects section because it is outdated

--- updateBlogScript.sh
+++ updateBlogScript.sh
@@ -3,7 +3,7 @@
 # Usage: When you want to update the blogs
 # $ ./updateBlogScript.sh
 
-# the "content" directory is a sym link to my Obsidian Workspace where I have a folder dedicated to my online posts
+# the "content" directory is a sym link to my Obsidian Workspace where I have a folder dedicated to my online posts``
 
 # Format the date as YYYY-MM-DD. You can adjust the date format as needed.
 TODAYS_DATE=$(date)
