d091c7525e1e7eed6863effcb78da90262d9da6b
ethanmorganumich <etcmo@umich.edu>
2024-12-24 03:04:49 -0500

    update the 'updateBlogScript'

--- updateBlogScript.sh
+++ updateBlogScript.sh
@@ -8,6 +8,17 @@
 # Format the date as YYYY-MM-DD. You can adjust the date format as needed.
 TODAYS_DATE=$(date)
 
+# move into the quartz folder
+cd ../quartz
+# build the blog
 npx quartz build -o blog
+
+# go back into website
+cd ../ethanmorgan.io
+# remove old blog
+rm -rf blog
+# move in new blog
+mv ../quartz/blog .
+
 git add blog
 git commit -m "Blog update $TODAYS_DATE"
